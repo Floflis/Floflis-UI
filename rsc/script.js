8a29d61ba7249338d7e25406004fb3e923d0f5c7
DaniellMesquita <baleiazoca@gmail.com>
2014-08-24 01:18:31 -0300

    A few trade changes

--- rsc/script.js
+++ rsc/script.js
@@ -1,3 +1,19 @@
 window.onload = function() {
 	document.getElementById("bootscreen").style.display = "none"
 }
+
+function godash() {
+	document.getElementById('dashboardbutton').style.display='block';
+}
+function destroygodash() {
+	document.getElementById('dashboardbutton').style.display='none';
+}
+function destroygodash() {
+	document.getElementById('dashboardbutton').style.display='none';
+}
+function whiteline() {
+	document.getElementById('handle-line').style.background='#fff';
+}
+function destroywhite() {
+	document.getElementById('handle-line').style.background='rgba(255, 255, 255, 0.5)';
+}
