20e2bb0199a26ab029f5afd27473e1f77cac0365
Daniell Mesquita <daniellplasma@gmail.com>
2015-10-05 06:05:23 -0300

    Fixed and better readable script.js

--- rsc/script.js
+++ rsc/script.js
@@ -1,1 +1,7 @@
-function whiteline(){document.getElementById("handle-line").style.background="#fff"}function destroywhite(){document.getElementById("handle-line").style.background="rgba(255, 255, 255, 0.5)"}
+function whiteline() {
+document.getElementById("handle-line").style.background="#fff"
+}
+
+function destroywhite() {
+document.getElementById("handle-line").style.background="rgba(255, 255, 255, 0.5)"
+}
