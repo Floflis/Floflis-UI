d694b4363cdb792cde6271686862aacc91b34ae5
Daniell Mesquita <daniellplasma@gmail.com>
2016-02-03 10:01:21 -0200

    Added initial implements to short app names

--- live/rsc/script.js
+++ live/rsc/script.js
@@ -1,3 +1,10 @@
+$(".app-title").each(function () {
+    text = $(this).text();
+    if (text.length > 12) {
+        $(this).html(text.substr(0, 186) + '<span class="elipsis">' + text.substr(186) + '</span><a class="elipsis" href="#">....</a>');
+    }
+});
+
 function whiteline() {
 document.getElementById("handle-line").style.background="#fff"
 }
