47d7b4680351dd758578198a1cc6c417e618d587
Daniell Mesquita <daniellplasma@gmail.com>
2016-04-17 15:42:05 -0300

    #183 Detect if running in Node.js

--- live/rsc/script.js
+++ live/rsc/script.js
@@ -29,6 +29,16 @@ function toggleFullScreen() {
   }  
 }
 
+var isNode = false;    
+if (typeof process === 'object') {
+  if (typeof process.versions === 'object') {
+    if (typeof process.versions.node !== 'undefined') {
+      isNode = true;
+      
+    }
+  }
+}
+
 $(".app-title").each(function () {
     text = $(this).text();
     if (text.length > 12) {
