ee504f4ac1df1cdef51b0b5dc11bdeec6a3e8fd2
Daniell Mesquita <daniellplasma@gmail.com>
2016-04-17 07:02:57 -0300

    #163 Fullscreen button in Test Hub

--- live/rsc/script.js
+++ live/rsc/script.js
@@ -6,6 +6,29 @@ $("li.navi-item").hover(function() {
 });
 });
 
+function toggleFullScreen() {
+  document.getElementById("btnFullscreen").className = "notifycenterbtn active";
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    document.getElementById("btnFullscreen").className = "notifycenterbtn";
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  }  
+}
+
 $(".app-title").each(function () {
     text = $(this).text();
     if (text.length > 12) {
