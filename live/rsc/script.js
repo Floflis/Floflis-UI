241f056f7e22336fea8040fc9278e8b917c88cf2
Daniell Mesquita <daniellplasma@gmail.com>
2016-04-24 18:32:23 -0300

    #149 Clock in boot screen

--- live/rsc/script.js
+++ live/rsc/script.js
@@ -6,22 +6,6 @@ $("li.navi-item").hover(function() {
 });
 });
 
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
-}
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
-
 function toggleFullScreen() {
   document.getElementById("btnFullscreen").className = "notifycenterbtn active";
   if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
