a3ad96b2d0ce2512c09d7a5fc578144941fe3f2c
Daniell Mesquita <daniellplasma@gmail.com>
2016-04-29 21:38:00 -0300

    fixed #248

--- live/System/Addons/battery/battery.js
+++ live/System/Addons/battery/battery.js
@@ -120,91 +120,81 @@
   root.battery = new Battery();
 })(this);
 
+(function() {
+  var el = {
+    charging: document.getElementById('charging'),
+    chargingTime: document.getElementById('chargingTime'),
+    dischargingTime: document.getElementById('dischargingTime'),
+    level: document.getElementById('level'),
+    battery: document.getElementById('battery'),
+    batteryBar: document.getElementById('battery-bar')
+  };
 
+  battery.on('ready', function() {
+    console.log('ready');
+    console.log('isCharging: ' + battery.isCharging());
+    console.log('isDischarging: ' + battery.isDischarging());
+    console.log('getChargingTime: ' + battery.getChargingTime());
+    console.log('getDischargingTime: ' + battery.getDischargingTime()); // seconds
+    console.log('getLevel: ' + battery.getLevel());
+    console.log('getPercentage: ' + battery.getPercentage());
+    console.log('getStatus: ' + battery.getStatus());
 
+ battery.on('chargingChange', updateCharging);
 
+ battery.on('chargingTimeChange', updateChargingTime);
 
+ battery.on('dischargingTimeChange', updateDischargingTime);
 
+ battery.on('levelChange', updateLevel);
 
-var $ = document;
-var el = {
-  charging: $.getElementById('charging'),
-  chargingTime: $.getElementById('chargingTime'),
-  dischargingTime: $.getElementById('dischargingTime'),
-  level: $.getElementById('level'),
-  batteryBar: $.querySelector('#battery-bar')
-};
+ battery.on('error', function(error) {
+   alert(error);
+   console.error(error);
+ });
 
-battery.on('ready', function() {
-  console.log('ready');
-  console.log('isCharging: ' + battery.isCharging());
-  console.log('isDischarging: ' + battery.isDischarging());
-  console.log('getChargingTime: ' + battery.getChargingTime());
-  console.log('getDischargingTime: ' + battery.getDischargingTime()); // seconds
-  console.log('getLevel: ' + battery.getLevel());
-  console.log('getPercentage: ' + battery.getPercentage());
-  console.log('getStatus: ' + battery.getStatus());
-
-  updateCharging(battery.isCharging());
-  updateChargingTime(battery.getChargingTime());
-  updateDischargingTime(battery.getDischargingTime());
-  updateLevel(battery.getLevel());
-});
-
-battery.on('chargingChange', updateCharging);
-
-battery.on('chargingTimeChange', updateChargingTime);
-
-battery.on('dischargingTimeChange', updateDischargingTime);
-
-battery.on('levelChange', updateLevel);
-
-battery.on('error', function(error) {
-  alert(error);
-  console.error(error);
-});
-
-function updateCharging(isCharging) {
-  setText(el.charging, isCharging);
-}
-
-function updateChargingTime(chargingTime) {
-  setText(el.chargingTime, chargingTime + toHMS(chargingTime));
-}
-
-function updateDischargingTime(dischargingTime) {
-  setText(el.dischargingTime, dischargingTime + toHMS(dischargingTime));
-}
-
-function updateLevel(level) {
-  setText(el.level, level + toPercent(level));
-  var statusClass = 'ok';
-  if (level < 0.2) {
-    statusClass = 'warning';
+  function updateCharging(isCharging) {
+    setText(el.charging, isCharging);
   }
-  if (level < 0.1) {
-    statusClass = 'critical';
+
+  function updateChargingTime(chargingTime) {
+    setText(el.chargingTime, chargingTime + toHMS(chargingTime));
   }
-  el.batteryBar.setAttribute('width', battery.getPercentage() + '%');
-  el.batteryBar.setAttribute('class', statusClass);
-}
 
-function toPercent(value) {
-  return ' (' + (value * 100) + '%)';
-}
-
-function toHMS(durationInSeconds) {
-  if (!Number.isFinite(durationInSeconds)) return '';
-  var hours = Math.floor(durationInSeconds / 3600);
-  var seconds = durationInSeconds % 60;
-  var minutes = Math.floor((durationInSeconds - hours * 3600 - seconds) / 60);
-  return ' (' + hours + 'h:' + minutes + 'm:' + seconds + 's)';
-}
-
-function setText(el, t) {
-  if (document.all) {
-    el.innerText = t;
-  } else{
-    el.textContent = t;
+  function updateDischargingTime(dischargingTime) {
+    setText(el.dischargingTime, dischargingTime + toHMS(dischargingTime));
   }
-}
+
+  function updateLevel(level) {
+    setText(el.level, level + toPercent(level));
+    var statusClass = 'ok';
+    if (level < 0.2) {
+      statusClass = 'warning';
+    }
+    if (level < 0.1) {
+      statusClass = 'critical';
+    }
+    el.batteryBar.style.width = battery.getPercentage() + '%';
+    el.batteryBar.setAttribute('class', statusClass);
+  }
+
+ function toPercent(value) {
+   return ' (' + (value * 100) + '%)';
+ }
+
+ function toHMS(durationInSeconds) {
+   if (!Number.isFinite(durationInSeconds)) return '';
+   var hours = Math.floor(durationInSeconds / 3600);
+   var seconds = durationInSeconds % 60;
+   var minutes = Math.floor((durationInSeconds - hours * 3600 - seconds) / 60);
+   return ' (' + hours + 'h:' + minutes + 'm:' + seconds + 's)';
+ }
+
+  function setText(el, t) {
+    if (document.all) {
+      el.innerText = t;
+    } else{
+      el.textContent = t;
+    }
+  }
+})();
