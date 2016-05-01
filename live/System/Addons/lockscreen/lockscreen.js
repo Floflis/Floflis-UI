4ec008b88c1ba535ea2a1bcd06c23ae6bb248f01
Daniell Mesquita <daniellplasma@gmail.com>
2016-04-30 22:34:10 -0300

    #252 Brand new boot system

--- live/System/Addons/lockscreen/lockscreen.js
+++ live/System/Addons/lockscreen/lockscreen.js
@@ -4,3 +4,5 @@ document.getElementById("body").className = "unlocked";
 function lock() {
 document.getElementById("body").className = "locked";
 }
+var div = document.getElementById('body');
+div.innerHTML = div.innerHTML + '<div id="lockscreen" style="background:url(System/Resources/Themes/Default/Backgrounds/Lockscreen/Default.svg#default) no-repeat center center fixed" onclick="secondboot()"></div>';
