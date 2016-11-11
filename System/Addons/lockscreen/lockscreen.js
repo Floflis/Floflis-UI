2688748f1cc93034d3dbe344353e35c4edd10215
Daniell Plasmmer <daniellmesquito@gmail.com>
2016-11-11 04:19:05 -0200

    #275 Remove User Interface

--- System/Addons/lockscreen/lockscreen.js
+++
@@ -1,8 +0,0 @@
-function unlock() {
-document.getElementById("body").className = "unlocked";
-}
-function lock() {
-document.getElementById("body").className = "locked";
-}
-var div = document.getElementById('body');
-div.innerHTML = div.innerHTML + '<div id="lockscreen" style="background:url(System/Resources/Themes/Default/Backgrounds/Lockscreen/Default.svg#default) no-repeat center center fixed" onclick="secondboot()"></div>';
--- core.js
+++ core.js
@@ -1,11 +1,3 @@
 function restart() {
 location.reload();
 }
-
-function whiteline() {
-document.getElementById("handle-line").style.background="#fff"
-}
-
-function destroywhite() {
-document.getElementById("handle-line").style.background="rgba(255, 255, 255, 0.5)"
-}
