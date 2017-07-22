0e22d65b37e94c4b9a9363445286851e6f4fc95d
Plasmmer <plasmmer@outlook.com>
2017-07-22 19:07:31 -0300

    Fully changed WS name to Screens
    Signed-off-by: Plasmmer <plasmmer@outlook.com>

--- System/Addons/lockscreen/lockscreen.js
+++ System/Addons/lockscreen/lockscreen.js
@@ -1,12 +1,12 @@
 function unlock() {
 document.getElementById("body").className = "unlocked";
 body = document.createElement('audio');
-document.getElementById("title").innerHTML = "WS.js";
+document.getElementById("title").innerHTML = "Screens";
 body.innerHTML = '<audio id="sound" src="' + 'System/Addons/lockscreen/snd/System Logon.flac'+ '" autoplay></audio>'
 }
 function lock() {
 document.getElementById("body").className = "locked";
-document.getElementById("title").innerHTML = "Lockscreen - WS.js";
+document.getElementById("title").innerHTML = "Lockscreen - Screens";
 }
 var div = document.getElementById('body');
 div.innerHTML = div.innerHTML + '<div id="lockscreen" style="background:url(System/Resources/Themes/Default/Backgrounds/Lockscreen/Default.png) no-repeat center center fixed;    background-size:cover" onclick="secondboot()"></div>';
