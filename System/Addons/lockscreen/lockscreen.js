2e47f3d32189a6f05b4be4bd97d569e7a1915e62
Plasmmer <plasmmer@outlook.com>
2017-07-23 07:25:19 -0300

    Removed boot & addons feature to make it better in future
    Signed-off-by: Plasmmer <plasmmer@outlook.com>

--- System/Addons/lockscreen/lockscreen.js
+++ System/Addons/lockscreen/lockscreen.js
@@ -8,5 +8,3 @@ function lock() {
 document.getElementById("body").className = "locked";
 document.getElementById("title").innerHTML = "Lockscreen - Screens";
 }
-var div = document.getElementById('body');
-div.innerHTML = div.innerHTML + '<div id="lockscreen" style="background:url(System/Resources/Themes/Default/Backgrounds/Lockscreen/Default.png) no-repeat center center fixed;    background-size:cover" onclick="secondboot()"></div>';
